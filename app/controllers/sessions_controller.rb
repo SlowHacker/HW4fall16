3d59a79226d16504684a90507afc44bead10aa3a
Qiao <qiao-liang@uiowa.edu>
2016-09-30 23:32:22 +0000

    Fixed a bug that the login status message disappears

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,6 @@ class SessionsController < ApplicationController
         @user = User.where(user_id: user_params[:user_id], email: user_params[:email])
         if @user.present?
             session[:session_token] = @user.first.session_token
-            flash[:notice] = "You are logged in as #{@user.first.user_id}"
             redirect_to movies_path
         else
             flash[:notice] = "Invalid user-id/e-mail combination. Please try again."
